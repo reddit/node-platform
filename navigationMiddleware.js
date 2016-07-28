d33ec9aafb429fa754646bffbba0734a348201d0
Niranjan Ramadas <nrbramadas@gmail.com>
2016-07-28 16:37:25 -0700

    Rename `parseRoute`

--- navigationMiddleware.js
+++ navigationMiddleware.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-	exports.parseRoute = parseRoute;
+	exports.matchRoute = matchRoute;
 
 	var _pathToRegexp = __webpack_require__(321);
 
@@ -85,7 +85,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-	function parseRoute(path, routes) {
+	function matchRoute(path, routes) {
 	  var route = void 0;
 
 	  var _iteratorNormalCompletion = true;
@@ -135,7 +135,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  var dispatch = store.dispatch;
 	  var getState = store.getState;
 
-	  var route = parseRoute(pathName, routes);
+	  var route = matchRoute(pathName, routes);
 
 	  if (route) {
 	    var _ret = function () {
